728174196ecb4b52f51fd339a76aedb4ce6c916d
James Mason <jmason@yamatoengines.com>
2010-04-01 18:47:31 -0700

    used named scopes on attendance crap

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :coordinator, :class_name => "Person", :foreign_key => "coordinator_id"
   has_many :attendances
-  has_many :attending_people, :class_name => "Person", :through => :attendances, 
-    :conditions => {:attending=>'yes'}
-  has_many :declining_people, :class_name => "Person", :through => :attendances,
-    :conditions => {:attending=>'no'}
-  has_many :uncommitted_people, :class_name => "Person", :through => :attendances,
-    :conditions => 'attending NOT IN (\'yes\',\'no\')'
+  has_many :people, :through => :attendances
   
+  named_scope :attending, :conditions => {:attending=>'yes'}
+  named_scope :declining, :conditions => {:attending=>'no'}
+  named_scope :uncommitted, :conditions => 'attending NOT IN (\'yes\',\'no\')'
 end
