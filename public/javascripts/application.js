c03fb61ac6c0a4db4dc2fc9506bf154007988b0e
Nathan Carnes <nathan@nathancarnes.com>
2010-04-02 19:04:29 -0700

    Added Easy framework and Typekit; started stubbing out design a bit. Need to swap out background.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){	
+	$.easy.navigation();
+	$.easy.tooltip();
+	$.easy.popup();
+	$.easy.external();
+	$.easy.rotate();
+	$.easy.cycle();
+	$.easy.forms();
+	$.easy.showhide();
+	$.easy.jump();
+});
