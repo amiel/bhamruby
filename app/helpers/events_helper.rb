38b4a5271f4e6e59fba946a0e9c4713b832273ca
Amiel Martin <amiel.martin@gmail.com>
2010-04-24 15:09:36 -0700

    style events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def link_to_map title, address
-    link_to h(title), "http://maps.google.com/maps?q=#{url_encode @next_event.location_address.gsub(/\s/,' ')}"
+    link_to h(title), "http://maps.google.com/maps?q=#{url_encode address.gsub(/\s/,' ')}"
   end
 end
